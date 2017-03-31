5715b3c5585f307bce14763409f45641ccc9c443
ly <pass@word1>
2017-03-31 10:44:38 +0800

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { SharedModule } from './shared/shared.module';
-import { LoginComponent } from './workspace/login/login.component';
-import { appRoutes } from './app.routes';
-import { AuthGuard } from './guards/auth.guard';
-import { AuthenticationService } from './services/authentication.service';
-import { UserService } from './services/user.service';
-// used to create fake backend
-import { fakeBackendProvider } from './helpers/fake-backend';
-import { MockBackend, MockConnection } from '@angular/http/testing';
-import { BaseRequestOptions } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     FormsModule,
-    HttpModule,
-    RouterModule,
-    SharedModule,
-    RouterModule.forRoot(appRoutes)
+    HttpModule
   ],
-  providers: [
-    AuthGuard,
-    AuthenticationService,
-    UserService,
-    // providers used to create fake backend
-    fakeBackendProvider,
-    MockBackend,
-    BaseRequestOptions
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
