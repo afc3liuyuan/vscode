cb88b0ad83f8f2b511b666f53b7059e5a313aa2d
ly <pass@word1>
2017-03-31 10:24:36 +0800

    Revert "初始"
    This reverts commit 95ea9e47cee1a056c8103ee98e6cf248d31a3edf.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,10 @@
-import { Component, HostListener, ElementRef, Renderer, ViewContainerRef } from '@angular/core';
-import { ActivatedRoute, Router, ActivatedRouteSnapshot, RouterState, RouterStateSnapshot } from '@angular/router';
-import 'rxjs/add/operator/merge';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    private globalClickCallbackFn: Function;
-    private loginSuccessCallbackFn: Function;
-
-    constructor(
-        public elementRef: ElementRef,
-        public renderer: Renderer,
-        public router: Router,
-        public activatedRoute: ActivatedRoute,
-        public vcr: ViewContainerRef
-    ) {
-    }
-
-    ngOnInit() {
-        this.globalClickCallbackFn = this.renderer.listen(this.elementRef.nativeElement, 'click', (event: any) => {
-            console.log("全局监听点击事件>" + event);
-        });
-    }
-
-    ngOnDestroy() {
-        if (this.globalClickCallbackFn) {
-            this.globalClickCallbackFn();
-        }
-    }
+  title = 'app works!';
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,7 +1,4 @@
 /* SystemJS module definition */
-declare var System: any;
-declare var tinymce: any;
-declare var JQuery: any;
-declare var jQuery: any;
-declare var $: any;
-declare var require: any;
+declare var module: {
+  id: string;
+};
